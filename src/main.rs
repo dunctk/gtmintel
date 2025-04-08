42dbcfb2c381983ee480dc9b05f7ece9c69af882
Duncan <d@uncan.net>
2025-04-08 13:43:58 +0000

    x

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ async fn main() {
 
     // Run our server
     let app = create_app();
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    tracing::info!("Server running on http://127.0.0.1:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("Server running on http://0.0.0.0:3000");
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
